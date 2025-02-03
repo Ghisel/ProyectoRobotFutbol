0efc1675c41a166fb4baddaf4da651a8ce8cd5d6
Ghisel <giselasegovia532@gmail.com>
2025-02-03 18:43:42 -0300

    Descripción de los cambios realizados

--- proyectoRobotFutbol.ino
+++ proyectoRobotFutbol.ino
@@ -1,9 +1,75 @@
+int EnableA  =3; 
+int motorA1 = 5;
+int motorA2 = 6;
+int motorB1 = 9;
+int motorB2 = 10;
+int EnableB  =11;
+int ingreso;
+
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin (9600);
+  pinMode(EnableA, OUTPUT);
+  pinMode(EnableB, OUTPUT);
+  pinMode (motorA1, OUTPUT);
+  pinMode (motorA2, OUTPUT);
+  pinMode (motorB1, OUTPUT);
+  pinMode (motorB2, OUTPUT);  
+ 
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
+   
+  if (Serial.available()>0)
+  {
+  ingreso = Serial.read();
+  
+  if (ingreso == 'F') // 'a' adelante
+  {
+    
+  analogWrite (EnableA, 255);
+  digitalWrite (motorA1, HIGH);
+  digitalWrite (motorA2, LOW);
+  analogWrite (EnableB, 255);
+  digitalWrite (motorB1, HIGH);
+  digitalWrite (motorB2, LOW);
+  
 
+  }
+  if (ingreso == 'B') // 'r' atrás
+  {
+  analogWrite (EnableA, 255);
+  digitalWrite (motorA1, LOW);
+  digitalWrite (motorA2, HIGH);
+  analogWrite (EnableB, 255);
+  digitalWrite (motorB1, LOW);
+  digitalWrite (motorB2, HIGH);
+
+  }
+  if (ingreso == 'L') // DERECHA
+  {
+  analogWrite (EnableA, 255);
+  digitalWrite (motorA1, HIGH);
+  digitalWrite (motorA2, LOW);
+  analogWrite (EnableB, 255);
+  digitalWrite (motorB1, LOW);
+  digitalWrite (motorB2, HIGH);
+  }
+  if (ingreso == 'R') // IZQUIERDA
+  {
+  analogWrite (EnableA, 255);
+  digitalWrite (motorA1, LOW);
+  digitalWrite (motorA2, HIGH);
+  analogWrite (EnableB, 255);
+  digitalWrite (motorB1, HIGH);
+  digitalWrite (motorB2, LOW);
+  }  
+  if (ingreso == 'S') // 's' Stop
+  {
+    analogWrite (EnableA, 0);
+  digitalWrite (motorA1, LOW);
+  digitalWrite (motorA2, LOW);
+  analogWrite (EnableB, 0);
+  digitalWrite (motorB1, LOW);
+  digitalWrite (motorB2, LOW);
+  }  
+  }
 }
